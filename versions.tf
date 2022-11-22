752ea6ee3da65824cffc837119847d4ecb0f8d10
Keir Badger <keir.badger@acuris.com>
2022-11-22 10:24:37 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,4 +1,15 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    fastly = {
+      version = "0.41.0"
+      source = "fastly/fastly"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
 }
